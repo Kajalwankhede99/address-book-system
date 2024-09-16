695fdb13378370e9a58336bea433089b3453fbb8
Kajalwankhede <kajuwankhede151@gmail.com>
2024-09-16 14:57:24 +0530

    UC1- Ability to create Contacts in AddressBook with first and lastname,address,city,zip,phone and email.

---
+++ src/main/java/com/bridgelabz/addressbook/Contacts.java
@@ -0,0 +1,88 @@
+package com.bridgelabz.addressbook;
+
+public class Contacts {
+
+    private String firstname;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private int phoneNumber;
+    private String email;
+
+    public Contacts(String firstname, String lastName, String address, String city, String state, int zip, int phoneNumber, String email) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
